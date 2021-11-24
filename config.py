622a8263ad0b0fa54118d25113165d56b7244eb6
Manoj Kumawat <imkumawatmanoj@gmail.com>
2021-11-24 19:13:53 +0530

    Added config.py file

---
+++ config.py
@@ -0,0 +1,5 @@
+#Config file
+
+project_name = "Project Name: Instagram Media Grabber"
+owner = "Owner: Manoj Kumawat"
+Description = "Description: Contain varius functions to \ndownload media file from Instagram"
